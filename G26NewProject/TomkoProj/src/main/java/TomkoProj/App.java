a88e15aba652b5d40d72d59da06dc1bf124d4385
Taras <radulenko@mail.ru>
2017-07-03 00:23:21 +0300

    projects were added

---
+++ G26NewProject/TomkoProj/src/main/java/TomkoProj/App.java
@@ -0,0 +1,13 @@
+package TomkoProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
