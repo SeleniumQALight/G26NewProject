ff195817c0188b24cd4a9ea8811c24231178d6f2
Taras <radulenko@mail.ru>
2017-07-02 23:50:55 +0300

    projects were added

---
+++ G26NewProject/MorosovaProj/src/main/java/MorosovaProj/App.java
@@ -0,0 +1,13 @@
+package MorosovaProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
