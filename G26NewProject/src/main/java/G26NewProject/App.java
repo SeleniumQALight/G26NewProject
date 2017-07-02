7c389b82d9df41ba5fcaa4b648ed5d17886648ca
Taras <radulenko@mail.ru>
2017-07-02 22:42:35 +0300

    New Project

---
+++ G26NewProject/src/main/java/G26NewProject/App.java
@@ -0,0 +1,13 @@
+package G26NewProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
