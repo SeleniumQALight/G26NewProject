54257927c5fa947da8ee92b4c106f29096e6c323
Morozova Anna <g.26.anna.a@gmail.com>
2017-07-17 12:02:40 +0300

    New properties

--- G26NewProject/MorosovaProj/src/test/java/LoginTest/LoginTestWithoutPageObject.java
+++ G26NewProject/MorosovaProj/src/test/java/LoginTest/LoginTestWithoutPageObject.java
@@ -1,6 +1,10 @@
 package LoginTest;
 
 import org.junit.Test;
+<<<<<<< Updated upstream
+=======
+import org.openqa.selenium.By;
+>>>>>>> Stashed changes
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,11 +19,19 @@ public class LoginTestWithoutPageObject
     private WebDriver webDriver;
 
     @Test
-    public void validLogon()
+    public void validLogOn()
     {
         File fileFF = new File(".././drivers/chromedriver.exe");
+<<<<<<< Updated upstream
         System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
         webDriver = new ChromeDriver();
+=======
+
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+
+        webDriver = new ChromeDriver();
+
+>>>>>>> Stashed changes
         webDriver.manage().window().maximize();
 
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -30,8 +42,75 @@ public class LoginTestWithoutPageObject
 
         webDriver.quit();
 
+<<<<<<< Updated upstream
     }
 
 
 
 }
+=======
+
+    }
+
+    @Test
+    public void invalidLogOn()
+    {
+        File fileNN = new File( ".././drivers/chromedriver.exe");
+
+        System.setProperty("webdriver.chrome.driver", fileNN.getAbsolutePath());
+
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        webDriver.get("http://v3.qalight.com.ua/");
+
+        System.out.println("Page was opened");
+
+        webDriver.findElement(By.xpath(".//input[@name='_username']")).sendKeys("mmm");
+
+        webDriver.findElement(By.xpath(".//input[@name='_password']")).sendKeys("111");
+
+        webDriver.findElement(By.xpath(".//div[@class='col-xs-4']")).click();
+
+        System.out.println("Enter invalid password and email");
+
+        webDriver.findElement(By.xpath(".//img[@class='img-circle']"));
+
+        System.out.println("Log in with incorrect password");
+
+
+}
+
+    @Test
+    public void validLogin() {
+        File fileMM = new File(".././drivers/chromedriver.exe");
+
+        System.setProperty("webdriver.chrome.driver", fileMM.getAbsolutePath());
+
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        webDriver.get("http://v3.qalight.com.ua/");
+
+        System.out.println("Page was opened");
+
+        webDriver.findElement(By.xpath(".//*[@name='_username]")).sendKeys("Student");
+
+        webDriver.findElement(By.xpath(".//input[@name='_password']")).sendKeys("909090");
+
+        System.out.println("Enter valid Password and Email");
+
+        webDriver.findElement(By.xpath(".//img[@class='img-circle']"));
+
+        webDriver.quit();
+    }
+
+
+    }
+>>>>>>> Stashed changes
