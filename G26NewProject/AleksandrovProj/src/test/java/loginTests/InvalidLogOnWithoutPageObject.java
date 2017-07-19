fba4240ba99bd64fec3287d529128ef600fe2f18
andrey.aleksandrov <a.s.aleksandrov89@gmail.com>
2017-07-19 21:40:27 +0300

    ParentPage, LoginPage added; Login test updated with pageObject pattern

--- G26NewProject/AleksandrovProj/src/test/java/loginTests/InvalidLogOnWithoutPageObject.java
+++ G26NewProject/AleksandrovProj/src/test/java/loginTests/InvalidLogOnWithoutPageObject.java
@@ -1,6 +1,5 @@
 package loginTests;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import parentTest.ParentTest;
@@ -16,14 +15,18 @@ public class InvalidLogOnWithoutPageObject extends ParentTest {
 
     @Test
     public void invalidLogOn() {
-        webDriver.get( "http://v3.qalight.com.ua/" );
-        webDriver.findElement( By.xpath( "//input[@name='_username']" ) )
-                .sendKeys( "student" );
-        webDriver.findElement( By.xpath( "//input[@id='password']" ) )
-                .sendKeys( "wrongPassword" );
-        webDriver.findElement( By.xpath( "//button" ) ).click();
-        Assert.assertTrue( webDriver.findElement( By.xpath( "//*[contains(text(),'Учет запчастей')]" ) ).isDisplayed() );
+        logInPage.openLoginPage();
+//        webDriver.get( "http://v3.qalight.com.ua/" );
+        logInPage.enterLoginToInput("student");
+//        webDriver.findElement( By.xpath( "//input[@name='_username']" ) )
+//                .sendKeys( "student" );
+//        webDriver.findElement( By.xpath( "//input[@id='password']" ) )
+//                .sendKeys( "wrongPassword" );
+//        webDriver.findElement( By.xpath( "//button" ) ).click();
+//        Assert.assertTrue( webDriver.findElement( By.xpath( "//*[contains(text(),'Учет запчастей')]" ) ).isDisplayed() );
+        checkAcceptanceCriteria( "Text 'Учет запчастей' not found", webDriver.findElement( By.xpath( "//*[contains(text(),'Учет запчастей')]" ) ).isDisplayed() , true);
+
     }
-    
+
 }
 
--- G26NewProject/AleksandrovProj/src/test/java/parentTest/ParentTest.java
+++ G26NewProject/AleksandrovProj/src/test/java/parentTest/ParentTest.java
@@ -1,18 +1,23 @@
 package parentTest;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LoginPage;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.is;
 
 /**
  * Created by andrey.aleksandrov on 17.07.2017.
  */
 public class ParentTest {
     public WebDriver webDriver;
+    public LoginPage logInPage;
 
     public ParentTest() {
 
@@ -25,10 +30,15 @@ public class ParentTest {
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS );
+        logInPage = new LoginPage( webDriver );
     }
 
     @After
     public void tearDown() {
         webDriver.quit();
+    }
+
+    public void checkAcceptanceCriteria(String message, boolean actualResult, boolean expectedResult){
+        Assert.assertThat( message, actualResult, is(expectedResult) );
     }
 }
