d3b618af69ca258b4fbf21eb5a46dc32f939dfb0
Olga <happy@gmail.com>
2017-07-19 18:05:43 +0300

    Test LogOn was added second time new.

--- G26NewProject/TomkoProj/src/test/java/LoginTests/LoginTestWithOutPageObject.java
+++ G26NewProject/TomkoProj/src/test/java/LoginTests/LoginTestWithOutPageObject.java
@@ -18,7 +18,6 @@ public class LoginTestWithOutPageObject {
         webDriver = new ChromeDriver(); // это объект для реализации хром драйвера, тутмы открыли браузер но смаленьким экраном
         webDriver.manage().window().maximize(); // тут мы изменяем размер экрана до максимального
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // задаем время ожидания. наш браузер будет на протяжении 10с искать логин -элемент, если не найдет за 10с то будет ексепшин
-
         webDriver.get("http://v3.qalight.com.ua/");//get метод вставляет наш url в строку браузера
         System.out.println("Page was opened/"); // что мы знали что страница открылась
         webDriver.quit(); // закрытие вкладки и сам браузер
