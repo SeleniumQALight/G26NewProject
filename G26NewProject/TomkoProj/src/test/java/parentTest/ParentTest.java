6b65fc14ee5ec0742c68ce5c5f53779dea948aef
Olga <happy@gmail.com>
2017-07-20 20:47:41 +0300

    Created constructor.

--- G26NewProject/TomkoProj/src/test/java/parentTest/ParentTest.java
+++ G26NewProject/TomkoProj/src/test/java/parentTest/ParentTest.java
@@ -1,24 +1,30 @@
-package LoginTests;
+package parentTest;
 
-import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-public class LoginTestWithOutPageObject {
+public class ParentTest {
     WebDriver webDriver; // объявили объеkт -браузера
 
-    @Test
-    public void  validLogOn(){ // validLogOn будет валидный тест кейс
+    public ParentTest(WebDriver webDriver) { // создали конструтор наведя курсор на ParentTest и нажали Alt+Ins, создали чтобы могли использовать его в тестах
+        this.webDriver = webDriver;
+    }
+
+    @Before
+    public void setUp(){
         File fileFF = new File(".././drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
-        webDriver = new ChromeDriver(); // это объект для реализации хром драйвера, тутмы открыли браузер но смаленьким экраном
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath()); // этот путь нужен чтоб браузер знал от куда стартовать
+        webDriver = new ChromeDriver(); // тут откроется браузер, это как и команда ему открыться
         webDriver.manage().window().maximize(); // тут мы изменяем размер экрана до максимального
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // задаем время ожидания. наш браузер будет на протяжении 10с искать логин -элемент, если не найдет за 10с то будет ексепшин
-        webDriver.get("http://v3.qalight.com.ua/");//get метод вставляет наш url в строку браузера
-        System.out.println("Page was opened/"); // что мы знали что страница открылась
+    }
+    @After
+    public void tearDown(){
         webDriver.quit(); // закрытие вкладки и сам браузер
 
     }
