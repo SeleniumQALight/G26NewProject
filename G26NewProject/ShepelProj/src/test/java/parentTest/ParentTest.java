fa7539dfd5bfd5dd207b6c6da5d2e765af2b1d41
shepel_av <a.shepel@gmail.com>
2017-07-23 23:56:30 +0300

    Valid version

--- G26NewProject/ShepelProj/src/test/java/parentTest/ParentTest.java
+++ G26NewProject/ShepelProj/src/test/java/parentTest/ParentTest.java
@@ -1,21 +1,24 @@
 package parentTest;
 
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LogInPage;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by shepel_av on 17.07.2017.
- */
+import static org.hamcrest.CoreMatchers.is;
+
 public class ParentTest {
     public WebDriver webDriver;
+    public LogInPage logInPage;
 
-    public ParentTest(WebDriver webDriver) {
-        this.webDriver = webDriver;
+    public ParentTest() {
+
     }
 
     @Before
@@ -25,10 +28,15 @@ public class ParentTest {
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        logInPage = new LogInPage(webDriver);
     }
     @After
     public void tearDown() {
         webDriver.quit();
+    }
 
+    public void checkAC(String message, boolean actualResult
+            , boolean expectedResult){
+        Assert.assertThat(message,actualResult,is(expectedResult));
     }
 }
