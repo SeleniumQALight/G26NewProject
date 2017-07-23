2aaec69c500b09359d8dafb62756029acdfb0df7
shepel_av <a.shepel@gmail.com>
2017-07-23 23:54:02 +0300

    Valid version

--- G26NewProject/ShepelProj/src/main/java/libs/ActionsWithOurElements.java
+++ G26NewProject/ShepelProj/src/main/java/libs/ActionsWithOurElements.java
@@ -1,20 +1,19 @@
 package libs;
 
 
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 
+
 public class ActionsWithOurElements {
-    // Конструктор нужен для того, чтобы елемент webDriver, который прийдет из других классов, был сдесь обработан
-    // Тоже с логером
-    WebDriver webDriver; // добавляем сюда тот же драйвер, который работает в других классах
+    WebDriver webDriver;
     Logger logger;
     public ActionsWithOurElements(WebDriver webDriver) {
         this.webDriver = webDriver;
-        logger= Logger.getLogger(getClass()); // Добавляем логер
+        logger = Logger.getLogger(getClass());
     }
 
     /**
@@ -22,38 +21,33 @@ public class ActionsWithOurElements {
      * @param element
      * @param text
      */
-    // Создаем метот, который будет вводить текст в любой элемент и чиститься
-    public void enterText(WebElement element, String text) {
-        try{
+    public void enterText(WebElement element, String text){
+        try {
             element.clear();
             element.sendKeys(text);
             logger.info(text + " was inputted");
-        }catch (Exception e) {
-            logger.error("Can not work with element" + element);
-            Assert.fail("Can not work with element" + element);
+        }catch (Exception e){
+            logger.error("Can not work with element " + element);
+            Assert.fail("Can not work with element " + element);
         }
-
     }
 
     public void clickOnElement(WebElement element) {
-        try {
+        try{
             element.click();
             logger.info("element was clicked");
-
-        } catch (Exception e) {
-            logger.error("Can not work with element" + element);
-            Assert.fail("Can not work with element" + element);
+        }catch (Exception e){
+            logger.error("Can not work with element " + element);
+            Assert.fail("Can not work with element " + element);
         }
     }
 
-    public boolean isElementWithTextPresent(String locatorWithText) {
-        try {
-            webDriver.findElement(By.xpath(locatorWithText));
-        }catch (Exeptions e){
-            returns false;
+    public boolean isElementPresent(String locatorWithText) {
+        try{
+            return webDriver.findElement(By.xpath(locatorWithText))
+                    .isDisplayed();
+        }catch (Exception e) {
+            return false;
         }
-
-        return actionsWithOurElements.isElementPresent(".//*[contains(text(),'" + text + "')]");// Локатор универсальный на поиск любого текста на любой пейдже
     }
 }
-
