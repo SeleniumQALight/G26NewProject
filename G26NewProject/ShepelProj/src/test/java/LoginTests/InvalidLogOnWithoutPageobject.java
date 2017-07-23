4a860c2a82af6c4f72bcfaa45343d616bda23e0b
shepel_av <a.shepel@gmail.com>
2017-07-23 22:50:39 +0300

    Not finished

--- G26NewProject/ShepelProj/src/test/java/LoginTests/InvalidLogOnWithoutPageobject.java
+++ G26NewProject/ShepelProj/src/test/java/LoginTests/InvalidLogOnWithoutPageobject.java
@@ -1,16 +1,8 @@
 package LoginTests;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import parentTest.ParentTest;
-
-import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by shepel_av on 17.07.2017.
@@ -24,11 +16,17 @@ public class InvalidLogOnWithoutPageobject extends ParentTest{
 
     @Test
     public void invalidLogOn() {
-        webDriver.get("http://v3.qalight.com.ua/");
-        webDriver.findElement(By.xpath(".//*[@name='_username']")).sendKeys("student");
-        webDriver.findElement(By.xpath(".//*[@name='_password']")).sendKeys("xxx");
-        webDriver.findElement(By.xpath(".//button")).click();
-        Assert.assertTrue("Assert is working",webDriver.findElement(By.xpath(".//*[text()='Учет запчастей']")).isDisplayed()); // делаем проверку віведен ли на єкран текст
+        logInPage.openLoginPage();
+//        webDriver.findElement(By.xpath(".//*[@name='_username']")).sendKeys("Student");
+        logInPage.enterPasswordToInput("2222");
+//        webDriver.findElement(By.xpath(".//*[@name='_password']")).sendKeys("2222");
+        logInPage.clickOnSubmitButton();
+//        webDriver.findElement(By.xpath(".//button")).click();
+//        Assert.assertTrue("Assert is working",webDriver.findElement(
+//                By.xpath(".//*[text()='Учет запчастей']")).isDisplayed()); // делаем проверку віведен ли на єкран текст
+        checkAC("Text 'Учет запчастей' not found"),
+                logInPage.isElementWithTextPresent("Учет запчастей"),
+                true);
 
     }
 
