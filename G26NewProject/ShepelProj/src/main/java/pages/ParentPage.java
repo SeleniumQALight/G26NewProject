02b2bcba1a1fa71b822327d4750b487a31ccd48f
shepel_av <a.shepel@gmail.com>
2017-07-23 23:56:03 +0300

    Valid version

--- G26NewProject/ShepelProj/src/main/java/pages/ParentPage.java
+++ G26NewProject/ShepelProj/src/main/java/pages/ParentPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+
 import libs.ActionsWithOurElements;
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -15,11 +17,19 @@ public class ParentPage {
         logger = Logger.getLogger(getClass());
         actionsWithOurElements = new ActionsWithOurElements(webDriver);
         PageFactory.initElements(webDriver,this);
-
     }
 
-    public void open(String url) {
-
-
+    /**
+     * Method opens url
+     * @param url
+     */
+    public void open(String url){
+        try{
+            webDriver.get(url);
+            logger.info("Page was opened " + url);
+        }catch (Exception e){
+            logger.error("Page can not opened " + url);
+            Assert.fail("Page can not opened " + url);
+        }
     }
 }
