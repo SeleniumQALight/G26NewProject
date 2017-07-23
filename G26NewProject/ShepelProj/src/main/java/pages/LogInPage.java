fb163d1211a18d97d0759e0ac522bf50cb80888a
shepel_av <a.shepel@gmail.com>
2017-07-23 23:55:33 +0300

    Valid version

--- G26NewProject/ShepelProj/src/main/java/pages/LogInPage.java
+++ G26NewProject/ShepelProj/src/main/java/pages/LogInPage.java
@@ -1,41 +1,46 @@
 package pages;
 
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LogInPage extends ParentPage {
-    @FindBy(name = "_username") // ищем по названию поле
-    private WebElement loginInput; // создает объект и записывает в него значение из строки @FindBy(name = "_username")
+public class LogInPage extends ParentPage{
+    @FindBy(name = "_username")
+    private WebElement loginInput;
 
     @FindBy(id = "password")
-    private WebElement inputPass; // создает объект и записывает в него значение из строки @FindBy(id = "password")
+    private WebElement inputPass;
 
     @FindBy(xpath = ".//button")
-    private WebElement buttonSubmit; // создает объект и записывает в него значение из строки @FindBy(xpath = ".//button")
+    private WebElement buttonSubmit;
 
-    public LogInPage(WebDriver webDriver) { super(webDriver); }
+    public LogInPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void openLoginPage(){
+        open("http://v3.qalight.com.ua/");
+    }
 
     /**
      * Method input login to input
      * @param login
      */
-
-    public void openLoginPage() {
-        open("http://v3.qualight.com.ua/"); }
-
     public void enterLoginToInput(String login) {
-        actionsWithOurElements.enterText(loginInput,login); // выбираем alt+enter inputfield
-        try{
-            loginInput.sendKeys(login);
-            logger.info(login + "was inputed into Input Login");
-        }catch (Exception e) {
-            logger.error("Can ");
-        }
+        actionsWithOurElements.enterText(loginInput,login);
+//        try{
+//            loginInput.sendKeys(login);
+//            logger.info(login + " was inputed into Input Login");
+//        }catch (Exception e){
+//            logger.error("Can not work with input");
+//            Assert.fail("Can not work with input");
+//        }
     }
 
-    public void enterPasswordToInput(String passWord) {
+    public void enterPassWordToInput(String passWord){
         actionsWithOurElements.enterText(inputPass,passWord);
     }
 
@@ -43,5 +48,8 @@ public class LogInPage extends ParentPage {
         actionsWithOurElements.clickOnElement(buttonSubmit);
     }
 
+    public boolean isElementWithTextPresent(String text) {
 
+        return actionsWithOurElements.isElementPresent(".//*[contains(text(),'"+text+"')]");
+    }
 }
