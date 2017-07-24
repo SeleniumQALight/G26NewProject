2cd212ed018f2cc99a5e2d4eea010e1044496141
Morozova Anna <g.26.anna.a@gmail.com>
2017-07-24 21:35:53 +0300

    Merge branch 'master' of github.com:SeleniumQALight/G26NewProject into Morozova2107
    # Conflicts:
    #	G26NewProject/MorosovaProj/pom.xml

---
+++ G26NewProject/MorosovaProj/src/main/java/pages/HomePage.java
@@ -0,0 +1,24 @@
+package pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends ParentPage
+{
+
+    @FindBy (xpath = ".//img[@class='user-image']")
+    WebElement rightAvatar;
+
+    public HomePage(WebDriver webDriver)
+    {
+        super(webDriver);
+    }
+
+
+    public boolean isAvatarPresent()
+    {
+        return actionsWithOurElements.isElementPresent(rightAvatar);
+    }
+}
