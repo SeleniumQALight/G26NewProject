ab8145e3de7b520acd2ffe89a40ef7617158d17e
Kotsiuba Ilya <kotsiuba01@gmail.com>
2017-07-24 21:35:58 +0300

    ScreenShot method was added

--- G26NewProject/KotsiubaProj/src/test/java/parrentTest/ParrentTest.java
+++ G26NewProject/KotsiubaProj/src/test/java/parrentTest/ParrentTest.java
@@ -1,10 +1,15 @@
 package parrentTest;
 
+import libs.Utiles;
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import pages.LoginPage;
 
 import java.io.File;
@@ -19,13 +24,44 @@ public class ParrentTest {
     public WebDriver webDriver;
     // добавили в public что б LoginPage loginPage был виден везде
     public LoginPage loginPage;
+    // Обьявляем HomePage
+    public HomePage homePage;
+
+
+
+    // обьявляем дл СКРИНШОТОВ:
+    // мы обьявили логгер чтто б снизу его использовать в скриншотах
+    // getLogger - мы достаем с библиотеки ( это вместо System.out.println - мы используем)
+    private Logger logger = Logger.getLogger(getClass());
+
+    // создаем обьект утилс
+    private Utiles utiles = new Utiles();
+
+    // просто обьявлем стринговую переменную, тут будет содержаться изменяемый путь к файлам
+    private String pathToScreenShot;
+
+
+
+
 
     public ParrentTest() {
     }
 
+    // с помощью Junit
+    // благодаря @Rule Junit знает что это нужно выполнить
+@Rule
+public TestName testName = new TestName();
+
+
 
     @Before
     public void setUp() {
+        // абсолютный путь к нашему проекту. Мы создали обьект файлика, без указания пути где он находиться. Этот путь будет всегда меняться
+        File file = new File("");
+        // прописуем пусть сохранения: в папке Таргет создаться скриншот в ней соритироваться имя пекедж - имя класа - потом имя тест кейчас
+        pathToScreenShot = file.getAbsolutePath() + "\\target\\screenshots\\" + this.getClass().getPackage().getName() + "\\" + this.getClass().getSimpleName() + "\\" + this.testName.getMethodName() + ".jpg";
+
+
         File fileFF = new File(".././drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
         // webDriver - это интфейс описывающий работу с браузерами
@@ -33,17 +69,26 @@ public class ParrentTest {
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         loginPage = new LoginPage(webDriver);
+        homePage = new HomePage(webDriver);
     }
 
     @After
     public void tearDown() {
-        webDriver.quit();
+        //Дописуем. Если обьекта (драйвера нет) - то и нечего закрывать. Т.е. если он есть мы его закрываем
+        if (! (webDriver == null)) {
+            // дописуем скриншот!
+            utiles.screenShot(pathToScreenShot,webDriver);
+            webDriver.quit();
+        }
     }
 
 
     // проверяем залоигинились или нет. Даем ему месседж который будет выведен и А.Р. и Е.Р.
     // Метод который проверят!!
     public void checkAC(String message, boolean actualResult, boolean expectedResult) {
+
+
+
         // import static org.hamcrest.CoreMatchers.is;
         // ПРОВЕРКА АКТУАЛЬНОГО РЕЗУЛЬТАТА ПОЭТОМУ ОН В ParrentTest - проверка актуального и ожидаемого результата
         Assert.assertThat(message, actualResult, is(expectedResult));
