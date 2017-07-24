7433d9d7ae6aee1016bae680d249f864729322db
Kompantsev <kompancevegor@gmail.com>
2017-07-24 21:39:50 +0300

    ActionsWithOurElements -> text improve in logger add ValidLoginTest add Utils (screenshots) add HomePage ParentTest -> add Screenshot Rule and

--- G26NewProject/KompantsevProj/src/test/java/LoginTests/ValidLoginTest.java
+++ G26NewProject/KompantsevProj/src/test/java/LoginTests/ValidLoginTest.java
@@ -1,4 +1,15 @@
 package LoginTests;
 
-public class ValidLoginTest {
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class ValidLoginTest extends ParentTest {
+    @Test
+    public void validLogOn() {
+        logInPage.openLoginPage();
+        logInPage.enterLoginToInput("Student");
+        logInPage.enterPassWordToInput("909090");
+        logInPage.clickOnSubmitButton();
+        checkAC("Avatar isn`t present on page", homePage.isAvatarPresent(), true);
+    }
 }
