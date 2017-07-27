34cca98c4654820c820f2c330d9f4e2c742affd4
Morozova Anna <g.26.anna.a@gmail.com>
2017-07-27 21:53:51 +0300

    New Class: AddNewSpare, SparePage, Utils

---
+++ G26NewProject/MorosovaProj/src/test/java/spare/AddNewSpare.java
@@ -0,0 +1,25 @@
+package spare;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+
+public class AddNewSpare extends ParentTest
+{
+        @Test
+        public void addNewSpare()
+        {
+            logInPage.loginUser ("Student", "909090");
+            homePage.clickOnMenuDictionary();
+            homePage.clickOnSubMenuSpares();
+            sparePage.checkTitle("Учет запчастей");
+            sparePage.checkTitleH1PresentOnPageWithText("Запчасти Список");
+            sparePage.clickOnButtonPlus();
+            addNewSparePage.checkTitleH1PresentOnPageWithText("Словарь");
+            addNewSparePage.enterSpareName("Spare1");
+            addNewSparePage.selectElementFromDDSpareType("Датчики івіфв");
+            addNewSparePage.clickButtonCreate();
+            addNewSparePage.checkTitleH1PresentOnPageWithText("Запчасти Список");
+            checkAC("Element can not find spare in list ", sparePage.isSpareInList("Spare1"), true);
+        }
+}
