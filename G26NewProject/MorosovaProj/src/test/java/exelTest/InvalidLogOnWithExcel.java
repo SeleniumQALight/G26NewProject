d4a9edc950c8adfc7ea5d22dad6b94fc426e4127
Morozova Anna <g.26.anna.a@gmail.com>
2017-08-01 19:42:19 +0300

    New Class: ProviderPage, CheckCheckbox. Create new pkgs provider, data with excel data

---
+++ G26NewProject/MorosovaProj/src/test/java/exelTest/InvalidLogOnWithExcel.java
@@ -0,0 +1,30 @@
+package exelTest;
+
+
+import libs.ExcelDriver;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import parentTest.ParentTest;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class InvalidLogOnWithExcel extends ParentTest
+{
+    private ExcelDriver excelDriver;
+    Logger logger = Logger.getLogger(getClass());
+
+    @Test
+    public void invalidLogOnWithExcel ()
+            throws IOException {
+        excelDriver = new ExcelDriver();
+        Map <String, String> dataSet = excelDriver.getData(".//src//main//java//data//testData.xls",
+                "validLogOn");
+        logger.info(dataSet);
+
+        logInPage.openLoginPage();
+        logInPage.enterLogInToInput(dataSet.get("login"));
+        logInPage.enterPasswordToInput(dataSet.get("pass"));
+        logInPage.clickOnSubmitButton();
+    }
+}
---
+++ G26NewProject/MorosovaProj/src/test/java/providers/CheckCheckbox.java
@@ -0,0 +1,11 @@
+package providers;
+
+
+import org.openqa.selenium.support.FindBy;
+import pages.ParentPage;
+import parentTest.ParentTest;
+
+public class CheckCheckbox extends ParentTest
+{
+
+}
