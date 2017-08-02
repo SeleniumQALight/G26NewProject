84d9c119eac0c78c8b8ff8bae04cf341771748ce
hush2627 <hush2627@gmail.com>
2017-08-02 19:39:15 +0300

    SuiteLoginPage

---
+++ G26NewProject/StepanenkoProj/src/test/java/suites/SuiteLoginPage.java
@@ -0,0 +1,29 @@
+package suites;
+
+
+import LoginTests.InvalidLogInWithoutPageObject;
+import LoginTests.LoginTestWithoutPageObject;
+import LoginTests.ValidLogInTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @RunWith(Suite.class) аннотация показывает начало работы со сьютом
+ */
+@RunWith(Suite.class)
+
+/**
+ * @Suite.SuiteClasses аннотация перечисляет все тесты в сьюите
+ */
+@Suite.SuiteClasses({
+
+        ValidLogInTest.class,
+        InvalidLogInWithoutPageObject.class,
+        LoginTestWithoutPageObject.class
+
+}
+
+)
+
+public class SuiteLoginPage {
+}
