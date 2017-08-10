749a9343eaf23fb28000a76eb4397f59ac092abc
Taras Radulenko <Taras_Radulenko@epam.com>
2017-08-10 14:19:01 +0300

    path to chromeDriver was added to jBehave project

--- G26NewProject/jBehaveProject/src/test/java/mainPack/jbehave/AcceptanceTestSuite.java
+++ G26NewProject/jBehaveProject/src/test/java/mainPack/jbehave/AcceptanceTestSuite.java
@@ -4,7 +4,7 @@ import net.thucydides.jbehave.ThucydidesJUnitStories;
 
 public class AcceptanceTestSuite extends ThucydidesJUnitStories {
 	public AcceptanceTestSuite() {
-		System.setProperty("webdriver.chrome.driver", "D:\\workspaceG26\\G26NewProject\\G26NewProject\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "..\\drivers\\chromedriver.exe");
 		findStoriesCalled("**/*.story");
 	}
 }
