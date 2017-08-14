cc9046c69a0462cd574bd8b5421d4096609de20d
Kotsiuba Ilya <kotsiuba01@gmail.com>
2017-08-14 20:43:38 +0300

    Work SeleniumGrid

--- G26NewProject/KotsiubaProj/src/test/java/loginTests/LoginTestWithOutPageObject.java
+++ G26NewProject/KotsiubaProj/src/test/java/loginTests/LoginTestWithOutPageObject.java
@@ -5,8 +5,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 
@@ -22,10 +26,12 @@ public class LoginTestWithOutPageObject {
     }
 
     @Test
-    public void validLogOn() {
+    public void validLogOn() throws MalformedURLException {
         File fileFF = new File(".././drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
-        webDriver = new ChromeDriver();
+        // комитем для SeleniumGrid
+       // webDriver = new ChromeDriver();
+        webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.get("http://v3.qalight.com.ua/login");
