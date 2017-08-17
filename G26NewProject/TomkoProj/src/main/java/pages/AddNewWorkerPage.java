42b1e576f326668b371439bac853f1d1c085b359
Olga <happy@gmail.com>
2017-08-17 15:23:28 +0300

    Methods enterWorkerSurname and enterWorkerMiddleName were added.

--- G26NewProject/TomkoProj/src/main/java/pages/AddNewWorkerPage.java
+++ G26NewProject/TomkoProj/src/main/java/pages/AddNewWorkerPage.java
@@ -8,8 +8,14 @@ import org.openqa.selenium.support.FindBy;
  * Created by 1 on 15.08.2017.
  */
 public class AddNewWorkerPage extends ParenPage{
+    @FindBy(id="workers_workerSurname")
+    private WebElement workerSurnameInput;
+
     @FindBy(id="workers_workerName")
-    private WebElement workerNameInput;
+    private WebElement workerNameInput; //where to input
+
+    @FindBy(id="workers_workerMiddleName")
+    private WebElement workerMiddleNameInput; //where to input
 
     @FindBy(name="save")
     private WebElement buttonSave;
@@ -17,8 +23,14 @@ public class AddNewWorkerPage extends ParenPage{
     public AddNewWorkerPage(WebDriver webDriver) {
         super(webDriver);
     }
+    public void enterWorkerSurname(String workerSurname) {
+        actionsWithOurElements.enterText(workerSurnameInput, workerSurname);
+    }
     public void enterWorkerName(String workerName){
         actionsWithOurElements.enterText(workerNameInput,workerName); // в параметрах (куда внести , что внести)
+    }
+    public void enterWorkerMiddleName(String workerMiddleName){
+        actionsWithOurElements.enterText(workerMiddleNameInput,workerMiddleName);
     }
     public void clickOnButtonSave(){
         actionsWithOurElements.clickOnElement(buttonSave);
