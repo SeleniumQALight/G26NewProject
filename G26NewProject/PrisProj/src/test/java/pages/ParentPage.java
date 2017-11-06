37910472d47e5b64d4518e574331170ef0081570
RainbowG26 <lem.feather@yandex.ua>
2017-11-06 12:33:00 +0200

    fix class LoginTestWithOutPageObject - change name package loginTests(correct import) 06/11/17

---
+++ G26NewProject/PrisProj/src/test/java/pages/ParentPage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import libs.ActionWithOurElements;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
+/**
+ * Created by Dmitriy on 19.07.2017.
+ */
+//Сюда будем выносить все обищие страницы
+public class ParentPage {
+    WebDriver webDriver;
+    Logger logger;
+    ActionWithOurElements actionWithOurElements;
+
+    public ParentPage(WebDriver webDriver) { //Создали конструктор для того чтобы передать webDriver
+        this.webDriver = webDriver; //webDriver перетащит webDriver который перейдет из вне
+        logger = Logger.getLogger(getClass());
+        //создали обьект благодаря которому в каждой Page мы будем с ним работать
+        actionWithOurElements = new ActionWithOurElements(webDriver);
+        //инициализировать елементы(@FindBy) того класса который к ней обращается через оператор this
+        PageFactory.initElements(webDriver, this);
+    }
+
+    /**
+     * Method opens url
+     * @param url
+     */
+    public void open(String url){ //создаем метод open и в него передаем Тип String с именем url
+        try {
+            webDriver.get(url); //открываем страницу
+            logger.info("Page was opened" + url);
+        }catch (Exception e){
+            logger.error("Page can not opened" + url);
+            //Assert.fail пишет текст в junit ЛОГ который будет использоваться для отчетов, и помимо этого он тест останавливает
+           //Assert любой из них при негативном тесте завершает работы теста и пишет красным
+            Assert.fail("Page can not opened" + url);
+        }
+    }
+}
